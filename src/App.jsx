69eb2ab42c7356105ba4f304efb3487b60637e4c
abirraiyan <raiyan.abir112@gmail.com>
2024-07-11 13:11:37 +0600

    shoe card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,80 @@
 import Card from "./Card";
 
 export default function App() {
+
+
+  const cardData=[{
+    name: "Nike",
+    price: "20000",
+    des:" Nike World Is your Price",
+    imageUrl:"https://c0.klipartz.com/pngpicture/723/143/gratis-png-zapato-nike-air-force-zapatos-nike-thumbnail.png"
+  },
+  
+  {
+    name: "Adidas",
+    price: "25000",
+    des:" Adidas boost 350",
+    imageUrl:"https://storage.apex4u.com/1f2429b4-beda-4c52-971d-545c90ec2bc2.png"
+  },
+  {
+    name: "Bata",
+    price: "2000",
+    des:" World Class Shoe",
+    imageUrl:"https://e7.pngegg.com/pngimages/626/431/png-clipart-bata-shoes-sneakers-dress-shoe-casual-heels-brown-retail.png"
+  },
+  {
+    name: "Apex",
+    price: "3000",
+    des:" Apex Shoe",
+    imageUrl:"https://e7.pngegg.com/pngimages/784/778/png-clipart-nike-sports-shoes-adidas-puma-tsugi-apex-evoknit-nike.png"
+  },
+  {
+    name: "Lotto",
+    price: "3000",
+    des:" Lotto Shoe",
+    imageUrl:"https://e7.pngegg.com/pngimages/784/778/png-clipart-nike-sports-shoes-adidas-puma-tsugi-apex-evoknit-nike.png"
+  },
+  {
+    name: "Nike",
+    price: "20000",
+    des:" Nike World Is your Price",
+    imageUrl:"https://c0.klipartz.com/pngpicture/723/143/gratis-png-zapato-nike-air-force-zapatos-nike-thumbnail.png"
+  },
+  
+  {
+    name: "Adidas",
+    price: "25000",
+    des:" Adidas boost 350",
+    imageUrl:"https://storage.apex4u.com/1f2429b4-beda-4c52-971d-545c90ec2bc2.png"
+  },
+  {
+    name: "Bata",
+    price: "2000",
+    des:" World Class Shoe",
+    imageUrl:"https://e7.pngegg.com/pngimages/626/431/png-clipart-bata-shoes-sneakers-dress-shoe-casual-heels-brown-retail.png"
+  },
+  {
+    name: "Apex",
+    price: "3000",
+    des:" Apex Shoe",
+    imageUrl:"https://e7.pngegg.com/pngimages/784/778/png-clipart-nike-sports-shoes-adidas-puma-tsugi-apex-evoknit-nike.png"
+  },
+  {
+    name: "Lotto",
+    price: "3000",
+    des:" Lotto Shoe",
+    imageUrl:"https://e7.pngegg.com/pngimages/784/778/png-clipart-nike-sports-shoes-adidas-puma-tsugi-apex-evoknit-nike.png"
+  },
+
+];
+console.log(cardData);
+
   return (
 
-    <div>
-    <Card/>
-    <Card/>
+    <div className="flex flex-wrap gap-20">
+      {
+        cardData.map((shoe, i) => (<Card cardInfo={shoe} key={i}/>))
+      }
     </div>
   )
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({cardInfo}) => {
+  const { name, des, imageUrl, price } = cardInfo;
   return (
     <div>
       <div className="card card-compact w-96 bg-base-100 shadow-xl">
-  <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <figure><img src={imageUrl} alt="Shoes" /></figure>
   <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <h2 className="card-title">{name}</h2>
+    <p>${price}</p>
+    <p>{des}</p>
     <div className="card-actions justify-end">
       <button className="btn btn-primary">Buy Now</button>
     </div>

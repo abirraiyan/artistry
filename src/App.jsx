5889fa1205f423b63b9fba889a6ed472ab932963
abirraiyan <raiyan.abir112@gmail.com>
2024-07-11 11:59:02 +0600

    card

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Card from "./Card";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world! after react
-      Neesa I love You
-    </h1>
+
+    <div>
+    <Card/>
+    <Card/>
+    </div>
   )
 }
